331934b7248499acf1a836da6fba13646266b4e5
Emilie <emilieeeef@gmail.com>
2019-03-04 16:38:51 +0100

    Problème pour affichage des coordonnées

--- src/Controller/OpenstreetmapController.php
+++ src/Controller/OpenstreetmapController.php
@@ -18,11 +18,27 @@ class OpenstreetmapController extends AbstractController
         $fileLocator = new FileLocator($configDirectories);
         $jsonUserFiles = $fileLocator->locate('recup-verre.json', null, false); -- */
 
-        $fp = fopen('C:\xampp\htdocs\EcoVerre\src\Repository\Json\recup-verre.geojson','r');
-        dump($fp);
+
+     //   $p= file_get_contents("https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=recup-verre");
+     //   $p = json_decode($p);
+     //   $p = get_object_vars($p);
+
+        // var_dump($p);
+       // var_dump(get_object_vars($p));
+
+        // $directory = $this->getParameter('kernel.root_dir') . '/Repository/json/';
+        // $fileLocator = new FileLocator($directory);
+         // $jsonFiles = $fileLocator->locate('recup-verre.geojson', null, false);
+
+
+         $fp = file_get_contents('C:\xampp\htdocs\EcoVerre\src\Repository\json\recup-verre.geojson');
+         $fp = json_decode($fp);
+         dump($fp);
+
 
         return $this->render('openstreetmap/index.html.twig', [
             'controller_name' => 'OpenstreetmapController',
+            'json' => $fp
         ]);
 
 
