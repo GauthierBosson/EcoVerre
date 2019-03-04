d5ab989bbcfbd06a05e3cda81f7a61325961bd40
Emilie <emilieeeef@gmail.com>
2019-03-04 13:40:31 +0100

    Affichage des données sur prototype HTML, maintenant afficher sur le twig

--- src/Controller/OpenstreetmapController.php
+++ src/Controller/OpenstreetmapController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Config\FileLocator;
+
 
 class OpenstreetmapController extends AbstractController
 {
@@ -12,8 +14,20 @@ class OpenstreetmapController extends AbstractController
      */
     public function index()
     {
+        /* --  $configDirectories = [__DIR__.'../../Repository/json'];
+        $fileLocator = new FileLocator($configDirectories);
+        $jsonUserFiles = $fileLocator->locate('recup-verre.json', null, false); -- */
+
+        $fp = fopen('C:\xampp\htdocs\EcoVerre\src\Repository\Json\recup-verre.geojson','r');
+        dump($fp);
+
         return $this->render('openstreetmap/index.html.twig', [
             'controller_name' => 'OpenstreetmapController',
         ]);
+
+
+
     }
+
+
 }
