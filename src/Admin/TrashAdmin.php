d63b8f67c3547937a6dc279438a561cc55bbd716
Gauthier <bosson.gauthier27@gmail.com>
2019-03-04 11:54:01 +0100

    Création d'une page d'admin bennes
    Configuration de Sonata :
    - Création de la classe TrashAdmin pour administrer les bennes
    - Modification de servives.yaml pour injecter les dépendances sonata

---
+++ src/Admin/TrashAdmin.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gauthierbosson
+ * Date: 04/03/2019
+ * Time: 11:40
+ */
+
+namespace App\Admin;
+
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class TrashAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name', TextType::class);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('name');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('name');
+    }
+}
