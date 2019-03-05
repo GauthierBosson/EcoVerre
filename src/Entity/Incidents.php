e47a1b32ca4d198b2b3702f1b6c72868e7161f96
Gauthier <bosson.gauthier27@gmail.com>
2019-03-05 15:25:28 +0100

    Modification de l'entitée incidents
    Suppression des champs suivants :
    - city
    - address
    - latitude
    - longitude
    - altitude
    
    Raison : un incident est lié à une benne, et la benne contient déjà ces éléments qui étaient donc des doublons

--- src/Entity/Incidents.php
+++ src/Entity/Incidents.php
@@ -37,31 +37,6 @@ class Incidents
     private $reference;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $city;
-
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $address;
-
-    /**
-     * @ORM\Column(type="float", nullable=true)
-     */
-    private $latitude;
-
-    /**
-     * @ORM\Column(type="float", nullable=true)
-     */
-    private $longitude;
-
-    /**
-     * @ORM\Column(type="float", nullable=true)
-     */
-    private $altitude;
-
-    /**
      * @ORM\Column(type="text")
      */
     private $description;
@@ -137,66 +112,6 @@ class Incidents
     public function setReference(string $reference): self
     {
         $this->reference = $reference;
-
-        return $this;
-    }
-
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
-
-    public function setCity(string $city): self
-    {
-        $this->city = $city;
-
-        return $this;
-    }
-
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
-
-    public function setAddress(string $address): self
-    {
-        $this->address = $address;
-
-        return $this;
-    }
-
-    public function getLatitude(): ?float
-    {
-        return $this->latitude;
-    }
-
-    public function setLatitude(?float $latitude): self
-    {
-        $this->latitude = $latitude;
-
-        return $this;
-    }
-
-    public function getLongitude(): ?float
-    {
-        return $this->longitude;
-    }
-
-    public function setLongitude(?float $longitude): self
-    {
-        $this->longitude = $longitude;
-
-        return $this;
-    }
-
-    public function getAltitude(): ?float
-    {
-        return $this->altitude;
-    }
-
-    public function setAltitude(?float $altitude): self
-    {
-        $this->altitude = $altitude;
 
         return $this;
     }
