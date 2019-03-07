8e51e9985d59e69b379e381f7f20a0c49f0f06f5
ClementAh <clement.allalah@gmail.com>
2019-03-07 11:46:12 +0100

    Correction des patterns du bot

--- src/Services/BotServices.php
+++ src/Services/BotServices.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cleme
- * Date: 05/03/2019
- * Time: 11:58
- */
 
 namespace App\Services;
 
@@ -13,13 +7,28 @@ class BotServices
 {
     public function handleHello()
     {
-        $responses = ['Va te faire'];
+        $responses = ['Bonjour','Salut, en quoi puis-je vous aider ?'];
 
         return $responses[array_rand($responses)];
     }
 
-    public function handleGoodbye(){
-        $responses = ['Oue c\'est sa casse toi','Au revoir'];
+
+    public function handleIncident()
+    {
+        $responses = ['Le rapport d\'incidents se trouve sur la fiche technique de la benne à verre concernée.'];
+        return $responses[array_rand($responses)];
+    }
+
+    public  function  handleReferend()
+    {
+        $responses = ['Le référend de votre ville est Théo Dacosta dit le malin','Le référend de votre ville est Jar jar binks, la clé de tout'];
+
+        return $responses[array_rand($responses)];
+    }
+
+    public function handleReferend2()
+    {
+        $responses = ['En cas d\'incidents sur une benne, vous pouvez le rapporter via le rapport d\'incidents qui se trouve sur profil de la benne concernée'];
 
         return $responses[array_rand($responses)];
     }
