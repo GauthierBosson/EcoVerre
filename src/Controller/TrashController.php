e13f2060fc0043476c597763c52b6507ea875922
theo <theodaco3@gmail.com>
2019-03-08 10:18:22 +0100

    Géneration id poubelle :
    Création d'une fonction générant  un identifiant poubelle en reprenant le nom de la commune le type de poubelle et un nombre

--- src/Controller/TrashController.php
+++ src/Controller/TrashController.php
@@ -27,30 +27,32 @@ class TrashController extends AbstractController
         if (isset($_POST['name'])) {
             $opts = array('http'=>array('header'=>"User-Agent: StevesCleverAddressScript 3.7.6\r\n"));
             $context = stream_context_create($opts);
-            $address = '25 rue du bec';
-            $zip = '76000';
+            $address = '25 rue du bec'; // changer en resultat de formulaire
+            $zip = '76000'; // changer en resultat de formulaire
             $url = 'https://nominatim.openstreetmap.org/search/25%20rue%20du%20bec%2076000?format=json&limit=1';
             $gps= file_get_contents($url,false,$context);
             $gps= json_decode($gps,true);
             $lat = $gps[0]['lat'];
             $lon = $gps[0]['lon'];
-            $communeName = 'Colomiers';
+            $communeName = 'Colomiers'; // changer en resultat de formulaire
+
             $commune = 'https://geo.api.gouv.fr/communes?nom='.$communeName;
             $commune = file_get_contents($commune);
             $commune = json_decode($commune, true);
-            var_dump($commune[0]);
             $code_com = $commune[0]['code'];
+            $idTrash = substr($communeName, 0 ,3). 'VE' . rand(100,999);
+            $idTrash = strtoupper($idTrash);
+            var_dump($idTrash);
             $name = $_POST['name'];
             $json ="{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[$lon,$lat},
-            \"properties\":{\"commune\":\"AUCAMVILLE\",\"adresse\":\"RUE DES CHENES\",\"code_com\":\"31022\",\"geo_point_2d\":[$lat,$lon],
-            \"dmt_type\":\"Verre\",\"id\":\"AUCVE5002\"}}";
+            \"properties\":{\"commune\":\"$communeName\",\"adresse\":\"$address\",\"code_com\":\"$code_com\",\"geo_point_2d\":[$lat,$lon],
+            \"dmt_type\":\"Verre\",\"id\":\"$idTrash\"}}";
 
 
             $po = file_get_contents('recup.js');
             $po = substr($po , 12);
-            var_dump($po);
             $po = json_decode($po , true);
-           $pa =  array_push($po[0]['features'],$json);
+            $pa =  array_push($po[0]['features'],$json);
             $pa = json_encode($po);
             $envoi = 'var Verre = '. $pa;
             $file = fopen('recup.js','a');
