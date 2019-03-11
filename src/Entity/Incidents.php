4f22bedeff80edcfdfbaac0bb7e2b750e63d0dd7
joffrey <joffrey-lhermitte@hotmail.fr>
2019-03-11 10:28:38 +0100

    Modification entité Incidents

--- src/Entity/Incidents.php
+++ src/Entity/Incidents.php
@@ -41,25 +41,6 @@ class Incidents
      */
     private $city;
 
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $address;
-
-    /**
-     * @ORM\Column(type="float", nullable=true)
-     */
-    private $latitude;
-
-    /**
-     * @ORM\Column(type="float", nullable=true)
-     */
-    private $longitude;
-
-    /**
-     * @ORM\Column(type="float", nullable=true)
-     */
-    private $altitude;
 
     /**
      * @ORM\Column(type="text")
@@ -153,53 +134,7 @@ class Incidents
         return $this;
     }
 
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
 
-    public function setAddress(string $address): self
-    {
-        $this->address = $address;
-
-        return $this;
-    }
-
-    public function getLatitude(): ?float
-    {
-        return $this->latitude;
-    }
-
-    public function setLatitude(?float $latitude): self
-    {
-        $this->latitude = $latitude;
-
-        return $this;
-    }
-
-    public function getLongitude(): ?float
-    {
-        return $this->longitude;
-    }
-
-    public function setLongitude(?float $longitude): self
-    {
-        $this->longitude = $longitude;
-
-        return $this;
-    }
-
-    public function getAltitude(): ?float
-    {
-        return $this->altitude;
-    }
-
-    public function setAltitude(?float $altitude): self
-    {
-        $this->altitude = $altitude;
-
-        return $this;
-    }
 
     public function getDescription(): ?string
     {
